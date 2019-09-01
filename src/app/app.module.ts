2b19f59a39f2d6a88df62914ceaa41bc04369d8a
pushkar srivastava <pushkar.srivastava@globallogic.com>
2019-09-01 21:14:57 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MaterialModule} from './material.module';
-import {AccomodationComponent} from './accomodation/accomodation.component';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialModule} from './material.module';
+import { AccomodationComponent} from './accomodation/accomodation.component';
 import { RoomComponent } from './accomodation/room/room.component';
 import { FoodAndBeverageComponent } from './food-and-beverage/food-and-beverage.component';
 import { RestaurantComponent } from './food-and-beverage/restaurant/restaurant.component';
@@ -23,6 +23,10 @@ import { WesternWeddingsComponent } from './weddings/western-weddings/western-we
 import { AuthComponent } from './auth/auth.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { NavtabsComponent } from './navigation/navtabs/navtabs.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -43,13 +47,18 @@ import { SignupComponent } from './auth/signup/signup.component';
     WesternWeddingsComponent,
     AuthComponent,
     SigninComponent,
-    SignupComponent
+    SignupComponent,
+    HomeComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    NavtabsComponent
   ],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import {MatBadgeModule} from '@angular/material/badge';
-import {MatButtonModule} from '@angular/material';
+import {MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTabsModule
+} from '@angular/material';
 
 @NgModule({
-    imports: [MatBadgeModule, MatButtonModule],
-    exports: [MatBadgeModule, MatButtonModule]
+    imports: [
+        MatBadgeModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+        MatTabsModule
+    ],
+    exports: [
+        MatBadgeModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+        MatTabsModule
+    ]
 })
 
 export class MaterialModule {}
